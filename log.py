3518debf2e0dde8e85d7158840f0641d6f85a7fb
Rodrigo Querino <rodrigoqfcosta@gmail.com>
2021-10-21 17:58:35 -0300

    Incialização do repositorio github

---
+++ log.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+from flask import jsonify
+from flask_cors import CORS
+
+import mariadb
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def test_connect():
+    # Connect to MariaDB Platform
+    try:
+        conn = mariadb.connect(user='root',
+                               password='pass123',
+                               host='localhost',
+                               port=3307,
+                               database='historic')
+    except:
+        return 'Error connecting to MariaDB'
+    return 'Connecting to MariaDB OK'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5003)
